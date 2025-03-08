80284cd6291f53f07c96e63c60b4dd4f9979e7d5
craymorgana <aaron.morgan.7@outloook.com>
2025-03-08 12:35:58 -0500

    fixed initial startup errors

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,38 +1,38 @@
-import decode from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
 
 class AuthService {
-	getProfile() {
-		return decode(this.getToken());
-	}
+  getProfile() {
+    return jwtDecode(this.getToken());
+  }
 
-	loggedIn() {
-		const token = this.getToken();
-		return token && !this.isTokenExpired(token) ? true : false;
-	}
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
 
-	isTokenExpired(token) {
-		const decoded = decode(token);
+  isTokenExpired(token) {
+    const decoded = jwtDecode(token);
 
-		if (decoded.exp < Date.now() / 1000) {
-			localStorage.removeItem('id_token');
-			return true;
-		}
-		return false;
-	}
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem("id_token");
+      return true;
+    }
+    return false;
+  }
 
-	getToken() {
-		return localStorage.getItem('id_token');
-	}
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
 
-	login(idToken) {
-		localStorage.setItem('id_token', idToken);
-		window.location.assign('/');
-	}
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
 
-	logout() {
-		localStorage.removeItem('id_token');
-		window.location.reload();
-	}
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.reload();
+  }
 }
 
 export default new AuthService();
